e373e1a0b616d6b5c633c35d82f94fa7b0622307
Tharun Balaji <tharunbalaji110@gmail.com>
2024-12-06 17:47:05 +0530

    created createAdminClient function

--- lib/appwrite/index.ts
+++ lib/appwrite/index.ts
@@ -1,5 +1,5 @@
 "use server";
-import { Account, Client, Databases } from "node-appwrite";
+import { Account, Avatars, Client, Databases, Storage } from "node-appwrite";
 import { appwriteConfig } from "./config";
 import { cookies } from "next/headers";
 
@@ -11,7 +11,7 @@ import { cookies } from "next/headers";
  * @returns An Appwrite client with the session set and shortcuts to the
  * Account and Databases services.
  */
-export const createSessionClient = async () => { 
+const createSessionClient = async () => {
   // Create an Appwrite client instance
   const client = new Client()
     .setEndpoint(appwriteConfig.endpointUrl) // The Appwrite endpoint
@@ -37,5 +37,46 @@ export const createSessionClient = async () => {
       return new Databases(client);
     },
   };
-
 };
+
+/**
+ * Creates an Appwrite client with the Appwrite secret key set. This client can
+ * be used to perform administrative tasks like creating users, creating
+ * databases, and creating storage buckets.
+ *
+ * @returns An Appwrite client with methods for the Account, Databases, Storage,
+ * and Avatars services.
+ */
+const createAdminClient = async () => {
+  const client = new Client()
+    .setEndpoint(appwriteConfig.endpointUrl) // The Appwrite endpoint
+    .setProject(appwriteConfig.projectId) // The Appwrite project ID
+    .setKey(appwriteConfig.secretKey); // The Appwrite secret key
+
+  // Return a client with methods for the Account, Databases, Storage, and
+  // Avatars services. This client is set up with the Appwrite secret key, so it
+  // can be used to perform administrative tasks.
+  return {
+    // A shortcut to the Account service
+    get account() {
+      return new Account(client);
+    },
+    // A shortcut to the Databases service
+    get databases() {
+      return new Databases(client);
+    },
+    // A shortcut to the Storage service
+    get storage() {
+      return new Storage(client);
+    },
+    // A shortcut to the Avatars service
+    get avatars() {
+      return new Avatars(client);
+    },
+  };
+};
+
+export {
+  createSessionClient,
+  createAdminClient
+}
