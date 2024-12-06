7db5cccd3afed71c2721647efa0f061741b2f243
Tharun Balaji <tharunbalaji110@gmail.com>
2024-12-06 18:21:55 +0530

    sign-up functionality completed

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -1,7 +1,172 @@
-import React from 'react'
+"use client";
 
-export default function AuthForm() {
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import Image from "next/image";
+import Link from "next/link";
+import { useState } from "react";
+import { createAccount } from "@/lib/actions/user.actions";
+
+type FormType = "sign-in" | "sign-up";
+
+  /**
+   * Defines the schema for the authentication form based on the type of form.
+   * For sign-up, the full name is required, while for sign-in, it is optional.
+   * @param {FormType} formType - The type of form. Either "sign-in" or "sign-up".
+   * @returns {import("zod").ZodObject} The schema for the authentication form.
+   */
+const authFormSchema = (formType: FormType) => {
+  return z.object({
+    email: z.string().email(),
+    fullName:
+      formType === "sign-up"
+        ? z.string().min(2).max(50)
+        : z.string().optional(),
+  });
+};
+
+
+export default function AuthForm({ type }: { type: FormType }) {
+
+  const [isLoading, setIsLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+  const [accountId, setAccountId] = useState(null);
+
+  const formSchema = authFormSchema(type);
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      fullName: "",
+      email: "",
+    },
+  });
+
+  // 2. Define a submit handler.
+  // Handles form submission.
+  async function onSubmit(values: z.infer<typeof formSchema>) {
+    // Mark the form as loading to prevent multiple submissions.
+    setIsLoading(true);
+    // Clear any existing error messages.
+    setErrorMessage("");
+
+    try {
+      // Depending on the type of form, either create a new user or sign in an existing one.
+      const user = await createAccount({
+        fullName: values.fullName || "",
+        email: values.email,
+      });
+      // Save the user's account ID to the state.
+      // This is used to log the user in after the account is created.
+      setAccountId(user.accountId);
+    } catch {
+      // If there's an error, display an error message.
+      setErrorMessage("Failed to create account. Please try again.");
+    } finally {
+      // Mark the form as not loading when the submission is complete.
+      setIsLoading(false);
+    }
+  }
+
   return (
-    <div>AuthForm</div>
-  )
+    <>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="auth-form">
+          <h1 className="form-title">
+            {type === "sign-in" ? "Sign In" : "Sign Up"}
+          </h1>
+          {type === "sign-up" && (
+            <FormField
+              control={form.control}
+              name="fullName"
+              render={({ field }) => (
+                <FormItem>
+                  <div className="shad-form-item">
+                    <FormLabel className="shad-form-label">Full Name</FormLabel>
+
+                    <FormControl>
+                      <Input
+                        placeholder="Enter your full name"
+                        className="shad-input"
+                        {...field}
+                      />
+                    </FormControl>
+                  </div>
+
+                  <FormMessage className="shad-form-message" />
+                </FormItem>
+              )}
+            />
+          )}
+
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <div className="shad-form-item">
+                  <FormLabel className="shad-form-label">Email</FormLabel>
+
+                  <FormControl>
+                    <Input
+                      placeholder="Enter your email"
+                      className="shad-input"
+                      {...field}
+                    />
+                  </FormControl>
+                </div>
+
+                <FormMessage className="shad-form-message" />
+              </FormItem>
+            )}
+          />
+
+          <Button
+            type="submit"
+            className="form-submit-button"
+            disabled={isLoading}
+          >
+            {type === "sign-in" ? "Sign In" : "Sign Up"}
+
+            {isLoading && (
+              <Image
+                src="/assets/icons/loader.svg"
+                alt="loader"
+                width={24}
+                height={24}
+                className="ml-2 animate-spin"
+              />
+            )}
+          </Button>
+          {errorMessage && <p className="error-message">*{errorMessage}</p>}
+
+          <div className="body-2 flex justify-center">
+            <p className="text-light-100">
+              {type === "sign-in"
+                ? "Don't have an account?"
+                : "Already have an account?"}
+            </p>
+            <Link
+              href={type === "sign-in" ? "/sign-up" : "/sign-in"}
+              className="ml-1 font-medium text-brand"
+            >
+              {" "}
+              {type === "sign-in" ? "Sign Up" : "Sign In"}
+            </Link>
+          </div>
+        </form>
+      </Form>
+    </>
+  );
 }
