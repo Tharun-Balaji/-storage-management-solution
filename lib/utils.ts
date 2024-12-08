127e33c73bf47e214d76187733d631b9b2e1165a
Tharun Balaji <tharunbalaji110@gmail.com>
2024-12-08 10:01:15 +0530

    created getFileIcon utility function

--- lib/utils.ts
+++ lib/utils.ts
@@ -73,4 +73,75 @@ const getFileType = (fileName: string) => {
   return { type: "other", extension };
 };
 
-export { getFileType, parseStringify };
+/**
+ * Takes a file extension and returns the path to the icon for that type of
+ * file. If the extension is not recognized, the type parameter is used to
+ * determine the icon to use.
+ *
+ * @param {string | undefined} extension The file extension to determine the
+ * icon for.
+ * @param {FileType | string} type The type of file to determine the icon for.
+ * If the extension is not recognized, the type parameter is used to determine
+ * the icon to use.
+ * @returns {string} The path to the icon for the given file type.
+ */
+const getFileIcon = (
+  extension: string | undefined,
+  type: FileType | string
+) => {
+  // Define a mapping of file extensions to icon paths
+  const extensionToIconMap: { [key: string]: string } = {
+    // Document icons
+    pdf: "/assets/icons/file-pdf.svg",
+    doc: "/assets/icons/file-doc.svg",
+    docx: "/assets/icons/file-docx.svg",
+    csv: "/assets/icons/file-csv.svg",
+    txt: "/assets/icons/file-txt.svg",
+    xls: "/assets/icons/file-document.svg",
+    xlsx: "/assets/icons/file-document.svg",
+    // Image icons
+    svg: "/assets/icons/file-image.svg",
+    // Video icons
+    mkv: "/assets/icons/file-video.svg",
+    mov: "/assets/icons/file-video.svg",
+    avi: "/assets/icons/file-video.svg",
+    wmv: "/assets/icons/file-video.svg",
+    mp4: "/assets/icons/file-video.svg",
+    flv: "/assets/icons/file-video.svg",
+    webm: "/assets/icons/file-video.svg",
+    m4v: "/assets/icons/file-video.svg",
+    "3gp": "/assets/icons/file-video.svg",
+    // Audio icons
+    mp3: "/assets/icons/file-audio.svg",
+    mpeg: "/assets/icons/file-audio.svg",
+    wav: "/assets/icons/file-audio.svg",
+    aac: "/assets/icons/file-audio.svg",
+    flac: "/assets/icons/file-audio.svg",
+    ogg: "/assets/icons/file-audio.svg",
+    wma: "/assets/icons/file-audio.svg",
+    m4a: "/assets/icons/file-audio.svg",
+    aiff: "/assets/icons/file-audio.svg",
+    alac: "/assets/icons/file-audio.svg"
+  };
+
+  // Check if the extension exists and return the corresponding icon path
+  if (extension && extensionToIconMap[extension]) {
+    return extensionToIconMap[extension];
+  }
+
+  // If no specific icon for the extension, use the type to determine the icon
+  switch (type) {
+    case "image":
+      return "/assets/icons/file-image.svg";
+    case "document":
+      return "/assets/icons/file-document.svg";
+    case "video":
+      return "/assets/icons/file-video.svg";
+    case "audio":
+      return "/assets/icons/file-audio.svg";
+    default:
+      return "/assets/icons/file-other.svg";
+  }
+};
+
+export { getFileType, parseStringify, getFileIcon };
