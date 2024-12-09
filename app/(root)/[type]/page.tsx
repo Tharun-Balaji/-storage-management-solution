f8b0408b258c028d2353ecb2b6665054a8528752
Tharun Balaji <tharunbalaji110@gmail.com>
2024-12-09 11:52:18 +0530

    added dynamic type route in root

---
+++ app/(root)/[type]/page.tsx
@@ -0,0 +1,16 @@
+
+import { SearchParamProps } from '@/types';
+import React from 'react'
+
+async function page({ params }: SearchParamProps) {
+
+  const type = ((await params)?.type as string) || "";
+
+  return <div className="page-container">
+      <section className="w-full">
+      <h1 className="h1 capitalize">{type}</h1>
+    </section>
+    </div>;
+}
+
+export default page
