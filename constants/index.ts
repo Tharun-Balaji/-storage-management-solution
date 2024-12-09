f2783b5158e540e3099b0ba9d74d9c99bb8c17b7
Tharun Balaji <tharunbalaji110@gmail.com>
2024-12-09 12:31:33 +0530

    added actionsDropdownItems constant

--- constants/index.ts
+++ constants/index.ts
@@ -30,4 +30,33 @@ const navItems = [
 ];
 
 const MAX_FILE_SIZE = 50 * 1024 * 1024; // 50MB
-export { avatarPlaceholderUrl, navItems, MAX_FILE_SIZE };
+
+const actionsDropdownItems = [
+  {
+    label: "Rename",
+    icon: "/assets/icons/edit.svg",
+    value: "rename",
+  },
+  {
+    label: "Details",
+    icon: "/assets/icons/info.svg",
+    value: "details",
+  },
+  {
+    label: "Share",
+    icon: "/assets/icons/share.svg",
+    value: "share",
+  },
+  {
+    label: "Download",
+    icon: "/assets/icons/download.svg",
+    value: "download",
+  },
+  {
+    label: "Delete",
+    icon: "/assets/icons/delete.svg",
+    value: "delete",
+  },
+];
+
+export { avatarPlaceholderUrl, navItems, MAX_FILE_SIZE, actionsDropdownItems };
