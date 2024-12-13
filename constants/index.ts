253f03d2e90ea565186de636eb9ebb658bb251c5
Tharun Balaji <tharunbalaji110@gmail.com>
2024-12-13 15:24:38 +0530

    created sortTypes constant

--- constants/index.ts
+++ constants/index.ts
@@ -59,4 +59,31 @@ const actionsDropdownItems = [
   },
 ];
 
-export { avatarPlaceholderUrl, navItems, MAX_FILE_SIZE, actionsDropdownItems };
+const sortTypes = [
+  {
+    label: "Date created (newest)",
+    value: "$createdAt-desc",
+  },
+  {
+    label: "Created Date (oldest)",
+    value: "$createdAt-asc",
+  },
+  {
+    label: "Name (A-Z)",
+    value: "name-asc",
+  },
+  {
+    label: "Name (Z-A)",
+    value: "name-desc",
+  },
+  {
+    label: "Size (Highest)",
+    value: "size-desc",
+  },
+  {
+    label: "Size (Lowest)",
+    value: "size-asc",
+  },
+];
+
+export { avatarPlaceholderUrl, navItems, MAX_FILE_SIZE, actionsDropdownItems,sortTypes };
